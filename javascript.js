2b131e87f0618dc3c0d749e906e8c862c4674aa4
Darío <dariorojo2018@gmail.com>
2023-02-01 23:16:33 -0300

    proyecto codo a codo

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function ver() {
+    document.getElementById('display-ver-ocultar').style.display = 'block';
+    document.getElementById('ver-display').style.display = 'none';
+    document.getElementById('ocultar-display').style.display = 'block';
+}
+function ocultar() {
+    document.getElementById('display-ver-ocultar').style.display = 'none';
+    document.getElementById('ocultar-display').style.display = 'none';
+    document.getElementById('ver-display').style.display = 'block';
+
+}
